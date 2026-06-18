157ab794d5bbf3e54fd1946af5002eb27ba44d2d
kilikisdoto <simonkilikis@gmail.com>
2026-06-18 18:00:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Database connection
 const pool = new Pool({
